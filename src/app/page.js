17f60ebd2fb2a115b1f01461777c4ff8d04a17ac
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-03-28 15:42:54 +0000

    feat:navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,31 @@
-'use client'
-import { Box, Container, DecorativeBox } from '@radix-ui/themes';
+import Link from 'next/link';
+import Banner from '../../public/imgs/banner.png'
 import Image from 'next/image';
-import ImageBanner from './components/banner.png'
 export default function Page() {
   return (
-    <Box style={{backgroundImage: `url(${ImageBanner.src})`,width: '100%',height: '100vh'}} className='h-screen bg-cover'>
-    <Container size="1">
-      sfdg
-    </Container>
-  </Box>
-       
+    <div className="navbar bg-base-100 text-white">
+    <div className="navbar-start">
+      {/*<div className="dropdown">
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+        </div>
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white">
+          <li><a>Homepage</a></li>
+          <li><a>Portfolio</a></li>
+          <li><a>About</a></li>
+        </ul>
+      </div>*/}
+
+    </div>
+    <div className="navbar-end">
+      <ul className='flex flex-row space-x-16'>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/">Portfolio</Link></li>
+        <li><Link href="/">Packages</Link></li>
+        <li><Link href="/">About</Link></li>
+        <li><Link href="/">Contact</Link></li>
+      </ul>
+    </div>
+  </div>
   );
 }
