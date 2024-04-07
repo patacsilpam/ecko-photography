8e33741f5ea86a261d319491c097249b820a26b4
patacsilpam <pamelampatacsil@gmail.com>
2024-04-07 21:18:09 +0800

    feat: nomination component

---
+++ src/app/components/nominations.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import Nominations from './../../../public/assets/nomination.webp'
+import Events from './../../../public/assets/events.webp'
+const nominations = () => {
+  return (
+    <div className='h-screen bg-white'>
+        <div>
+            <h1 className="text-4xl text-[#292929] text-center">Nominations and Event</h1>
+        </div>
+        <div className='grid place-items-center'>
+            <div className="carousel">
+                <div id="item1" className="carousel-item w-full">
+                    <Image src={Nominations} height={450} width={450} alt='nomination' className='m-auto'/>
+                </div> 
+                <div id="item2" className="carousel-item w-full">
+                    <Image src={Events} height={450} width={450} alt='award'  className='m-auto'/>
+                </div> 
+            </div> 
+            <div className="flex justify-center w-full py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a> 
+                <a href="#item2" className="btn btn-xs">2</a> 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default nominations
