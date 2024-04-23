72367856738e89f510cc0240f0749ce597a43088
patacsilpam <pamelampatacsil@gmail.com>
2024-04-23 20:57:03 +0800

    feature: stories

--- src/app/components/featured.jsx
+++ src/app/components/featured.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 const Tabs = () => {
   const [activeTab, setActiveTab] = useState(0);
   return (
-    <div className='h-screen bg-white flex flex-col '>
+    <div className='h-fit bg-white flex flex-col '>
       <div className='mt-20'>
         <h1 className='text-2xl md:text-4xl text-[#292929] text-center'>FEATURED STORIES</h1>
       </div>
@@ -25,17 +25,21 @@ const Tabs = () => {
           </button>
         ))}
       </div>
-      <div className="mt-4 grid grid-cols-3">
+      <div className="mt-4 grid md:grid-cols-3 grid-cols-1 place-items-center p-5">
         {
           tabs[activeTab].content.map((tab,index) =>
-            <div key={index}>
-                <p>{tab.story}</p>
-                <Image src={tab.image} alt={tab.story} width={250} height={250}/>
+            <div key={index} className='mb-10'>
+                
+                <Image src={tab.image} alt={tab.story} className='h-50 w-50' width={350} height={350}/>
+                <p className='text-center p-2'>{tab.story}</p>
             </div> 
           )
         }
+        
       </div>
-      <button className='bg-[#292929] text-white w-32 p-2 m-auto'>View All</button>
+      <div className='p-10 m-auto'>
+        <button className='bg-[#292929] text-white w-32 p-2'>View All</button>
+      </div>
     </div>
   );
 };
