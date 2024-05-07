ea05652cf1f62f6853fb3960025feb5fd0279ec2
patacsilpam <pamelampatacsil@gmail.com>
2024-05-07 21:29:41 +0800

    feat: portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,11 @@
+"use client"
 import React from 'react'
-
+import UnderConstruction from '../headers/underconstruction'
 const Portfolio = () => {
   return (
-    <div>portfolio</div>
+    <div>
+        <UnderConstruction/>
+    </div>
   )
 }
 
