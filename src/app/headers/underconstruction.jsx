c0cc6c1d25d6fb8a07245080807571628c2025c5
patacsilpam <pamelampatacsil@gmail.com>
2024-05-07 21:29:20 +0800

    feat:  underconstruction

---
+++ src/app/headers/underconstruction.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './navbar';
+const underconstruction = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default underconstruction
