36ba301052195982da0f7f869a0a33ef8f5708e5
patacsilpam <pamelampatacsil@gmail.com>
2024-05-09 22:49:04 +0800

    feat: adjust the text banner to the center

--- src/app/headers/underconstruction.jsx
+++ src/app/headers/underconstruction.jsx
@@ -3,10 +3,10 @@ import React, {useState} from 'react'
 
 const underconstruction = () => {
   return (
-    <div>
-       
-        <div className="">
-          <p className=''>This page is under construction</p>
+    <div className='text-gray-700'>
+        <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center">
+          <h1 className='text-4xl '>Coming Soon!</h1>
+          <p className='text-xl'>This page is on its way........................</p>
         </div>
     </div>
   )
