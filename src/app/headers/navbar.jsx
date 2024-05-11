a6848c7579c4c7a0ccc41758ab665824e1ae6874
patacsilpam <pamelampatacsil@gmail.com>
2024-05-11 23:23:32 +0800

    style: refactor navbar code

--- src/app/headers/navbar.jsx
+++ src/app/headers/navbar.jsx
@@ -4,22 +4,21 @@ import Link from 'next/link';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <nav className=''>
-      <div className="absolute bg-slate-600 bg-opacity-10 -z-1"></div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-        <div className="flex justify-between h-16">
-          <div className="flex-shrink-0 flex items-center">
-            <Link href="#" className="text-gray-700 text-2xl font-bold">Ecko Photography</Link>
+    <div className='fixed top-0 z-50'>
+      <div>
+        <nav className='flex flex-row justify-around w-screen mt-5'>
+          {/*LOGO <div className="absolute bg-slate-600 bg-opacity-10 -z-1"></div>*/}
+          <div>
+            <Link href="#" className="text-[#ffffff] text-2xl font-bold">Ecko Photography</Link>
           </div>
-          <div className="flex items-center">
-            <div className="hidden lg:block space-x-12">
-              <Link href="/" className="text-gray-700 hover:text-[#277764] px-3 py-2 rounded-md text-lg font-semibold">Home</Link>
-              <Link href="/portfolio" className="text-gray-700  hover:text-[#277764] px-3 py-2 rounded-md text-lg font-semibold">Portfolio</Link>
-              <Link href="/packages" className="text-gray-700  hover:text-[#277764] px-3 py-2 rounded-md text-lg font-semibold">Packages</Link>
-              <Link href="/about" className="text-gray-700 hover:text-[#277764] px-3 py-2 rounded-md text-lg font-semibold">About</Link>
-              <Link href="/contact" className="text-gray-700 hover:text-[#277764] px-3 py-2 rounded-md text-lg font-semibold">Contact</Link>
-            </div>
+          <div>
+            <ul className='hidden lg:block space-x-10'>
+              <Link href="/" className="text-[#ffffff] hover:text-[#277764] px-3 py-2 rounded-md text-xl">Home</Link>
+              <Link href="/portfolio" className="text-[#ffffff]  hover:text-[#277764] px-3 py-2 rounded-md text-xl ">Portfolio</Link>
+              <Link href="/packages" className="text-[#ffffff]  hover:text-[#277764] px-3 py-2 rounded-md text-xl ">Packages</Link>
+              <Link href="/about" className="text-[#ffffff] hover:text-[#277764] px-3 py-2 rounded-md text-xl ">About</Link>
+              <Link href="/contact" className="text-[#ffffff] hover:text-[#277764] px-3 py-2 rounded-md text-xl ">Contact</Link>
+            </ul>
             <div className="lg:hidden">
               <button onClick={() => setIsOpen(!isOpen)} className="text-white hover:text-gray-300 focus:outline-none">
                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -27,22 +26,25 @@ const Navbar = () => {
                 </svg>
               </button>
             </div>
+             {/* Mobile Menu */}
+       
           </div>
-        </div>
+          {/* */}
+        </nav>
+        {isOpen && (
+          <div className='fixed top-0 w-screen -z-50'>
+              <div className="lg:hidden z-20 bg-[#28282B] h-screen relative">
+              <div className="px-2 pt-2 pb-3 sm:px-3  flex flex-col justify-center items-center space-y-10 h-screen">
+                  <Link href="/" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">Home</Link>
+                  <Link href="/portfolio" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">Portfolio</Link>
+                  <Link href="/packages" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">Packages</Link>
+                  <Link href="/about" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">About</Link>
+                  <Link href="/contact" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">Contact</Link>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-      {/* Mobile Menu */}
-      {isOpen && (
-        <div className="lg:hidden z-20 bg-[#28282B] h-screen relative">
-          <div className="px-2 pt-2 pb-3 sm:px-3  flex flex-col justify-center items-center space-y-10 h-screen">
-              <Link href="/" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">Home</Link>
-              <Link href="/portfolio" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">Portfolio</Link>
-              <Link href="/packages" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">Packages</Link>
-              <Link href="/about" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">About</Link>
-              <Link href="/contact" className="text-[#FFFFFF]  hover:text-[#277764] p-2 rounded-md text-lg font-semibold">Contact</Link>
-          </div>
-        </div>
-      )}
-    </nav>
     </div>
   )
 }
