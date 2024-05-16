e2c9e294418cab46f847570778195791e19b7e1c
patacsilpam <pamelampatacsil@gmail.com>
2024-05-16 21:20:51 +0800

    feat:change text color

--- src/app/headers/underconstruction.jsx
+++ src/app/headers/underconstruction.jsx
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 
 const underconstruction = () => {
   return (
-    <div className='text-gray-700'>
+    <div className='text-gray-200'>
         <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center">
           <h1 className='text-4xl '>Coming Soon!</h1>
           <p className='text-xl'>This page is on its way........................</p>
